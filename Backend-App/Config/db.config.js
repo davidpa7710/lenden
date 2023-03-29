cbf0abcc0502d5e649b658e74fce69770d918007
David Palacios <davidpa7710@gmail.com>
2023-03-28 23:52:35 -0600

    fix: config security config database

--- Backend-App/Config/db.config.js
+++ Backend-App/Config/db.config.js
@@ -3,5 +3,5 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    url: process.env.DB_URI
+    url: process.env.DB_URI.replace('<password>', process.env.PASSWORD).replace('<database>', process.env.DB_NAME)
 }
